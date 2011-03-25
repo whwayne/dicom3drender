500e62046950d395cc4afb8e677bd6a3297ca44a
whwayne <whwayne@6cd6571a-b8a7-9731-76be-ad60d92c15d0>
2011-03-25 07:16:23 +0000

    3.25

--- trunk/ dicom3drender/dicom3DRender/src/edu/co/unal/bioing/jnukak3d/main2.java
+++ trunk/ dicom3drender/dicom3DRender/src/edu/co/unal/bioing/jnukak3d/main2.java
@@ -1,33 +1,9 @@
-/*
-  Copyright (C) 2009, Bioingenium Research Group
-  http://www.bioingenium.unal.edu.co
-  Author: Alexander Pinzon Fernandez
-
-  JNukak3D is free software; you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free
-  Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
-
-  JNukak3D is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-  License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with JNukak3D; if not, write to the Free Software Foundation, Inc.,
-  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA,
-  or see http://www.gnu.org/copyleft/gpl.html
-*/
-
 package edu.co.unal.bioing.jnukak3d;
 
 import javax.swing.SwingUtilities;
 
 
-/**
- *
- * @author Alexander Pinzon Fernandez
- */
+
 public class main2 {
     public static void main( String argv[]){
 
